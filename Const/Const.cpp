4699018c948577d039205bf1d37bc821efcd2ffc
ItzA13x <alex.vladiuc@gmail.com>
2023-02-22 21:00:34 +0000

    ‏‏‎ ‎

---
+++ Const/Const.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    // The const keyword specifies that a variable's value is constant
+    // This tells the compiler to prevent anything from modifying it
+    // (read-only) 
+    
+
+    const double PI = 3.14159; // It's good practice to change constant variable names to uppercase
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    std::cout << circumference << "cm";
+
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+    
+    
+    return 0;
+}
