b60cb133947a3eafddbe2d27ce6c528139068f6a
ItzA13x <alex.vladiuc@gmail.com>
2023-02-23 18:55:22 +0000

    ‏‏‎ ‎

---
+++ Typedef & Type Aliases/Typedef & Type Aliases.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t;
+
+typedef std::string text_t;
+
+
+int main(){
+
+    text_t firstName = "Alex";
+    std::cout << firstName;
+
+    return 0;
+}
